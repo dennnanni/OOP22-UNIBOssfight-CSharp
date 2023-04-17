3167bf186942984d023aa7b9db218c1282c73309
Sartini Matteo <matteo.sartini8@studio.unibo.it>
2023-04-17 15:58:31 +0200

    Tradotte classi Angle e Acceleration

---
+++ Sartini-Matteo/Acceleration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sartini_Matteo
+{
+    static class Acceleration
+    {
+
+        public static double Accelerate(double currentVelocity, double targetVelocity, double timeDelta)
+        {
+            if (currentVelocity == targetVelocity)
+            {
+                return targetVelocity;
+            }
+
+            double velocityDelta = targetVelocity - currentVelocity;
+
+            if (velocityDelta > timeDelta)
+            {
+                return currentVelocity + timeDelta;
+            }
+
+            if (velocityDelta < timeDelta)
+            {
+                return currentVelocity - timeDelta;
+            }
+
+            return targetVelocity;
+        }
+
+    }
+}
---
+++ Sartini-Matteo/Angle.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System;
+using System.Drawing;
+
+namespace Sartini_Matteo
+{
+    public static class Angle
+    {
+        public const int RIGHT_ANGLE = 90;
+
+        public static double FindAngle(Point point1, Point point2)
+        {
+            double dx = point2.X - point1.X;
+            double dy = point2.Y - point1.Y;
+            return -Math.Atan2(dy, dx);
+        }
+    }
+}
