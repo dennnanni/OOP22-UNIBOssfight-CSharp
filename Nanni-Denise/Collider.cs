b1df361ba212c17f8b7252adf220b08cc2bf220f
denise.nanni <denise.nanni1@gmail.com>
2023-04-19 10:17:32 +0200

    tradotto Collider

---
+++ Nanni-Denise/Collider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+     
+namespace Nanni_Denise
+{
+    ///
+    /// <inheritdoc/>
+    /// 
+    public class Collider : ICollider
+    {
+        private Dictionary<string, Action<IEntity>> _behaviours;
+
+        /// <summary>
+        /// Creates a new instance of the collider.
+        /// </summary>
+        public Collider() 
+        { 
+            _behaviours = new Dictionary<string, Action<IEntity>>();
+        }
+
+        ///
+        /// <inheritdoc/>
+        /// 
+        public void AddBehaviour(string key, Action<IEntity> action)
+        {
+            _behaviours.Add(key, action);
+        }
+
+        ///
+        /// <inheritdoc/>
+        /// 
+        public void AddBehaviours(IList<string> keys, Action<IEntity> action)
+        {
+            foreach (string key in keys)
+            {
+                AddBehaviour(key, action);
+            }
+        }
+
+        ///
+        /// <inheritdoc/>
+        /// 
+        public void ManageCollision(IEntity e)
+        {
+            if (_behaviours.ContainsKey(e.GetType().Name))
+            {
+                _behaviours[e.GetType().Name].Invoke(e);
+            }
+        }
+    }
+}
---
+++ Nanni-Denise/ICollider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nanni_Denise
+{
+    /// <summary>
+    /// This class models the collider component 
+    /// which manages the behaviours of the entities
+    /// on collisions.
+    /// </summary>
+    public interface ICollider
+    {
+        /// <summary>
+        /// Manages the collision executing the procedure associated 
+        /// to the colliding entityy.
+        /// </summary>
+        /// <param name="e">Entity with which the caller collides</param>
+        void ManageCollision(IEntity e);
+
+        /// <summary>
+        /// Associates a behaviour to a specific entity.
+        /// </summary>
+        /// <param name="key">string value of the entity</param>
+        /// <param name="action">behaviour</param>
+        void AddBehaviour(string key, Action<IEntity> action);
+
+        /// <summary>
+        /// Associates a behaviour to a list of entities.
+        /// </summary>
+        /// <param name="keys">a list of the string values of the entities</param>
+        /// <param name="action">behaviour</param>
+        void AddBehaviours(IList<string> keys, Action<IEntity> action);
+    }
+}
