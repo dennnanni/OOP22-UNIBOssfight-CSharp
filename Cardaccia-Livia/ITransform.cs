7deed79c6deee2e83ea0ed325ff394cb528857ba
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2023-04-21 11:39:37 +0200

    corretta interfaccia ITransform

--- Cardaccia-Livia/ITransform.cs
+++ Cardaccia-Livia/ITransform.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cardaccia_Livia
 {
-    internal interface ITransform
+    public interface ITransform
     {
+        /// <summary>
+        /// Changes the ground level.
+        /// </summary>
+        /// <param name="yGround">the new value of the ground</param>
+        void SetGroundLevel(double yGround);
+
+        /// <summary>
+        /// This method returns true if the entity is under the ground
+        /// level, false otherwise.
+        /// </summary>
+        /// <returns>true is the entity is under the ground level</returns>
+        Boolean IsUnderGroundLevel();
+
+        /// <summary>
+        /// Takes the entity back on the ground level if it is under it.
+        /// </summary>
+        void MoveOnGroundLevel();
+
+        /// <summary>
+        /// Translates the current position with the vector of components x and y.
+        /// </summary>
+        /// <param name="x">position on x-axis</param>
+        /// <param name="y">position on y-axis</param>
+        void Move(double x, double y);
+
+        /// <summary>
+        /// Moves the entity to a new position.
+        /// </summary>
+        /// <param name="x">new position on x-axis</param>
+        /// <param name="y">new position on y-axis</param>
+        void MoveTo(double x, double y);
     }
 }
