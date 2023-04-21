2e2d03424c80e359c34b85e6b252d7738994a91a
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2023-04-21 11:37:37 +0200

    tradotta classe Platform

--- Cardaccia-Livia/Platform.cs
+++ Cardaccia-Livia/Platform.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cardaccia_Livia
 {
-    class Platform
+    public class Platform
     {
+        private readonly ITransform _position;
+        private readonly int _height;
+        private readonly int _width;
+
+        /// <summary>
+        /// Creates a new instance of the class Wall.
+        /// </summary>
+        public Platform(ITransform Position, int Height, int Width)
+        {
+            _position = Position;
+            _height = Height;
+            _width = Width;
+        }
+
+        /// <summary>
+        /// This method is used to let the entity jump on the platform by
+        /// checking the collision on the topside of it and updating the
+        /// ground level.
+        /// </summary>
+        /// <param name="collidingEntity">the entity colliding</param>
+        /// <param name="platform">the platform on which the entity is jumping</param>
+        public static void Jump(IEntity collidingEntity, IEntity platform)
+        {
+            if (collidingEntity.Hitbox.GetCollisionSideOnY(platform.Position.Y) > 0
+                    && Math.Abs(collidingEntity.Hitbox.GetIntersectionOnX(platform))
+                    > Math.Abs(collidingEntity.Hitbox.GetIntersectionOnX(platform)))
+            {
+                collidingEntity.Behaviour
+                               .JumpingBehaviour
+                               .Invoke(collidingEntity, platform); 
+            }
+        }
     }
 }
