4fb63c6363faf320edda97eddad01d4ae82f810c
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2023-04-22 17:41:56 +0200

    tradotta interfaccia IBehaviour

--- Cardaccia-Livia/IBehaviour.cs
+++ Cardaccia-Livia/IBehaviour.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cardaccia_Livia
 {
-    interface IBehaviour
+    /// <summary>
+    /// This class models the Behaviour component
+    /// which determines the actions of the entities.
+    /// </summary>
+    public interface IBehaviour
     {
+        /// <summary>
+        /// Property to get and set the jumping behaviour.
+        /// </summary>
+        /// <returns></returns>
+        Action<IEntity, IEntity> JumpingBehaviour { get; set; }
+
+        /// <summary>
+        /// Property to get and set the bottom stopping behaviour.
+        /// </summary>
+        Action<IEntity, IEntity> BottomStoppingBehaviour { get; set; }
+
+        /// <summary>
+        /// Property to get and set the stopping behaviour.
+        /// </summary>
+        Action<IEntity, IEntity> SideStoppingBehaviour { get; set; }
     }
 }
