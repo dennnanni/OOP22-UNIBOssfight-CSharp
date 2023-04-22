0829a854cbd5ca0b1e328df7e770eeb51f3d692b
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2023-04-22 17:43:01 +0200

    tradotta interfaccia BehaviourBuilder

--- Cardaccia-Livia/IBehaviourBuilder.cs
+++ Cardaccia-Livia/IBehaviourBuilder.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cardaccia_Livia
+﻿namespace Cardaccia_Livia
 {
-    interface IBehaviourBuilder
+    /// <summary>
+    /// A Builder of the Behaviour component,
+    /// with methods used to add behaviours
+    /// and a method build() to be called just once.
+    /// </summary>
+    public interface IBehaviourBuilder
     {
+        /// <returns>this, and add a behaviour to jump
+        /// on platforms and walls</returns>
+        IBehaviourBuilder AddJumpOnTop();
+
+        /// <returns>this, and add a behaviour to stop when colliding
+        /// with the bottom of a wall<\returns>
+        IBehaviourBuilder AddStopFromBottom();
+
+        /// <returns>this, and add a behaviour to stop when colliding
+        /// with the side of a wall</returns>
+        IBehaviourBuilder AddStopFromSide();
+
+        /// <returns>the Behaviour built with all of the above</returns>
+        IBehaviour Build();
     }
 }
